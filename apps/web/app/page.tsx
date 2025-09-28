197792170bf61cbfb9c6e85de7d872b9cb65df31
Natt Nguyen <natt.lefevre@gmail.com>
2025-09-28 14:11:23 -0700

    feat: update Home component to include separate iframe sources for Button primary and controls in Storybook

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,14 +1,22 @@
 export default function Home() {
   const STORYBOOK = {
-    BUTTON:
-      "https://experiment-storybook-embed-storyboo.vercel.app/iframe.html?args=&globals=&id=example-button--primary&viewMode=story",
+    BUTTON: {
+      PRIMARY:
+        "https://experiment-storybook-embed-storyboo.vercel.app/iframe.html?args=&globals=&id=example-button--button-primary&viewMode=story",
+      CONTROLS:
+        "https://experiment-storybook-embed-storyboo.vercel.app/iframe.html?args=&globals=&id=example-button--button-controls&viewMode=story",
+    },
   }
 
   return (
     <div className="">
       <iframe
-        className="border border-gray-300 w-full min-h-[500px]"
-        src={STORYBOOK.BUTTON}
+        className="border border-gray-300 bg-transparent w-full min-h-[360px]"
+        src={STORYBOOK.BUTTON.PRIMARY}
+      />
+      <iframe
+        className="border border-gray-300 bg-transparent w-full min-h-[360px]"
+        src={STORYBOOK.BUTTON.CONTROLS}
       />
     </div>
   )
